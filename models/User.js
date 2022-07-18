a40afea80609f4bbdcb7328d682a2abf872e9656
Adam McLane <mclanea92@gmail.com>
2022-07-18 14:02:32 -0400

    added models

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      max_length: 50,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      match: [/.+@.+\..+/, 'Must match an email address!'],
+      
+    },
+    thoughts: {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought',
+    },
+    friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
